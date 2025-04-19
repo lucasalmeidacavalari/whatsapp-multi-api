568a018d0ba7ea47efa9903af9cbd1e7b905ea15
Lucas Cavalari <lucas.eduardo1203@hotmail.com>
2025-04-19 09:38:20 -0300

    melhorando index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+
+// Rotas
 import connectRoutes from "./routes/connectRoutes.js";
 import sessionRoutes from "./routes/sessionRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
@@ -7,10 +9,12 @@ import logoutRoutes from "./routes/logoutRoutes.js";
 import mediaRoutes from "./routes/mediaRoutes.js";
 import groupRoutes from "./routes/groupRoutes.js";
 
+// Configuração
 dotenv.config();
 const app = express();
 app.use(express.json());
 
+// Middleware de rotas
 app.use("/api", connectRoutes);
 app.use("/api", sessionRoutes);
 app.use("/api", messageRoutes);
@@ -18,11 +22,13 @@ app.use("/api", logoutRoutes);
 app.use("/api", mediaRoutes);
 app.use("/api", groupRoutes);
 
+// Rota base
 app.get("/", (req, res) => {
-  res.send("API WhatsApp Multi-Sessão funcionando!");
+  res.send("API WhatsApp Multi-Sessão SGBR funcionando!");
 });
 
+// Inicialização do servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`✅ Servidor rodando na porta ${PORT}`);
 });
